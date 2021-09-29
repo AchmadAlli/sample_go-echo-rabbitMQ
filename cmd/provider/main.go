06750fe70f3d1201d78a186ae1cf3d1f0eeac522
Achmad Ali B <achmadali.baidlowi@gmail.com>
2021-09-29 20:59:01 +0700

    send message to message broker

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"net/http"
 
+	rabbitMQConfig "github.com/achmadAlli/sample-go-echo-rabbitmq/config"
 	"github.com/labstack/echo"
+	"github.com/streadway/amqp"
 )
+
+type service struct {
+	rabbitmq *rabbitMQConfig.RabbitMQClient
+}
 
 func main() {
 	e := echo.New()
+
+	s := service{
+		rabbitmq: rabbitMQConfig.Connect(),
+	}
 
 	// initial route
 	e.GET("/", func(c echo.Context) error {
@@ -16,27 +26,56 @@ func main() {
 		})
 	})
 
-	e.GET("/send_1", sendToFirstConsumer)
-	e.GET("/send_2", sendToSecondConsumer)
-	e.GET("/send_3", sendToBothConsumer)
+	e.GET("/send_1", s.sendToFirstConsumer)
+	e.GET("/send_2", s.sendToSecondConsumer)
+	e.GET("/send_3", s.sendToBothConsumer)
 
 	e.Start(":8000")
 }
 
-func sendToFirstConsumer(c echo.Context) error {
+func (s service) sendToFirstConsumer(c echo.Context) error {
+	s.rabbitmq.DeclareQueue("FIRST_QUEUE")
+	message := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("message for first consumer"),
+	}
+
+	err := s.rabbitmq.PublishMessage("FIRST_QUEUE", message)
+
+	if err != nil {
+		panic(err)
+	}
 
 	return c.JSON(http.StatusOK, echo.Map{
 		"message": "message send to first consumer",
 	})
 }
 
-func sendToSecondConsumer(c echo.Context) error {
+func (s service) sendToSecondConsumer(c echo.Context) error {
+	s.rabbitmq.DeclareQueue("SECOND_QUEUE")
+
+	message := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("message for second consumer"),
+	}
+
+	s.rabbitmq.PublishMessage("SECOND_QUEUE", message)
+
 	return c.JSON(http.StatusOK, echo.Map{
 		"message": "message send to second consumer",
 	})
 }
 
-func sendToBothConsumer(c echo.Context) error {
+func (s service) sendToBothConsumer(c echo.Context) error {
+	s.rabbitmq.DeclareQueue("BOTH_QUEUE")
+
+	message := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("message for both consumer"),
+	}
+
+	s.rabbitmq.PublishMessage("BOTH_QUEUE", message)
+
 	return c.JSON(http.StatusOK, echo.Map{
 		"message": "message send to both consumer",
 	})
