ef8021cef8e144af2a7a1ed4288e027dd7faaf21
Achmad Ali B <achmadali.baidlowi@gmail.com>
2021-09-29 21:17:13 +0700

    consume queue

--- cmd/first_consumer/main.go
+++ cmd/first_consumer/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"log"
+
+	rabbitMQConfig "github.com/achmadAlli/sample-go-echo-rabbitmq/config"
+)
+
+type service struct {
+	rabbitmq *rabbitMQConfig.RabbitMQClient
+}
+
 func main() {
+
+	s := service{rabbitmq: rabbitMQConfig.Connect()}
+
+	messages, err := s.rabbitmq.Consume("FIRST_QUEUE")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// make channel to create infinite loop
+	forever := make(chan bool)
+
+	go func() {
+		for message := range messages {
+			log.Printf("# message from FIRST_QUEUES : %s\n", message.Body)
+		}
+	}()
+
+	<-forever
 
 }
--- cmd/secondary_consumer/main.go
+++ cmd/secondary_consumer/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"log"
+
+	rabbitMQConfig "github.com/achmadAlli/sample-go-echo-rabbitmq/config"
+)
+
+type service struct {
+	rabbitmq *rabbitMQConfig.RabbitMQClient
+}
+
+func main() {
+
+	s := service{rabbitmq: rabbitMQConfig.Connect()}
+
+	messages, err := s.rabbitmq.Consume("SECOND_QUEUE")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// make channel to create infinite loop
+	forever := make(chan bool)
+
+	go func() {
+		for message := range messages {
+			log.Printf("# message from SECOND_QUEUES : %s\n", message.Body)
+		}
+	}()
+
+	<-forever
+
+}
